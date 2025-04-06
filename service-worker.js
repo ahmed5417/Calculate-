78b4dd3003fa732190bed20633b9f6f36cec91a2
ahmed5417 <alfwadi44@gmail.com>
2025-04-06 03:04:02 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+// service-worker.js
+const CACHE_NAME = 'smart-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// تثبيت Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('تم فتح ذاكرة التخزين المؤقت');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// استراتيجية الاستجابة للطلبات: الشبكة أولاً، ثم التخزين المؤقت
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
+
+// تحديث التخزين المؤقت
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
